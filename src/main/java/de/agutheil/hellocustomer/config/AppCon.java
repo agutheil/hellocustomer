32c724498040259d9c2e7af8e387260ef33d1bad
agutheil <andreas.gutheil@gmail.com>
2014-03-06 22:23:20 +0100

    Java Config und Component Scan

--- src/main/java/de/agutheil/hellocustomer/config/AppCon.java
+++ src/main/java/de/agutheil/hellocustomer/config/AppCon.java
@@ -8,11 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
-import de.agutheil.hellocustomer.api.CustomerDAO;
-import de.agutheil.hellocustomer.api.CustomerService;
-import de.agutheil.hellocustomer.impl.DefaultCustomerService;
-import de.agutheil.hellocustomer.impl.H2CustomerDAO;
-
 @Configuration
 @ComponentScan(basePackages={"de.agutheil.hellocustomer.impl"})
 public class AppCon {
