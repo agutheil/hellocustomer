e2cfd2bec7497eaf81ff1ce23d0d6cbcfd809680
agutheil <andreas.gutheil@gmail.com>
2014-03-06 22:57:47 +0100

    Java Config ohne KomponentScan

---
+++ src/main/java/de/agutheil/hellocustomer/config/AppConfNoScan.java
@@ -0,0 +1,30 @@
+package de.agutheil.hellocustomer.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import de.agutheil.hellocustomer.api.CustomerDAO;
+import de.agutheil.hellocustomer.impl.DefaultCustomerService;
+import de.agutheil.hellocustomer.impl.H2CustomerDAO;
+
+@Configuration
+public class AppConfNoScan {
+	@Bean
+	DataSource dataSource(){
+		EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+	    builder.setType(EmbeddedDatabaseType.H2).addScript("schema.sql").addScript("test-data.sql");
+	    return builder.build();
+	}
+	@Bean
+	CustomerDAO customerDAO(DataSource dataSource){
+		return new H2CustomerDAO(dataSource);
+	}
+	@Bean
+	DefaultCustomerService customerService(CustomerDAO customerDAO){
+		return new DefaultCustomerService(customerDAO);
+	}
+}
